a4dafef4b5816557aea80aa340dd996c6667c858
Cecília Eduarda Coelho <ceciliaeduarda@live.com.pt>
2020-06-01 11:46:40 +0100

    Linked List with more functions

--- DS/linkedList.py
+++ DS/linkedList.py
@@ -1,45 +1,74 @@
+
+class Node():
+
+    def __init__(self , element):
+        self.get = element
+        self.next = None
+
 class LinkedList():
 
     def __init__(self):
-        self.linkedList = []
         self.head = None
-        self.current = 1
-
-    def isEmpty(self):
-
-        return(len(self.linkedList) == 0)
-
-    def node(self , element):
-
-        if self.isEmpty():
-            self.head = element
-            self.linkedList.append(element)
-        else:
-            self.linkedList.append(element)
-
-    def see_head(self):
-        print(self.head)
 
     def display(self):
-        print(self.linkedList)
+        cur = self.head
+        while cur != None:
+            print(cur.get)
+            cur = cur.next
 
-    def next(self ):
-        if self.current < len(self.linkedList):
-            print(self.linkedList[self.current])
-            self.current += 1
-        else:
-            print("No more values in linkedList")
+    def insert(self , element):
+        new = Node(element)
+        new.next = self.head
+        self.head = new
+
+    def append(self , element):
+        new = Node(element)
+        last = self.head
+        if self.head == None:
+            self.head = new
+        while last.next:
+            last = last.next
+        last.next = new
+
+    def custom_insert(self , element , position):
+        new = Node(element)
+        new.next = position.next
+        position.next = new
+
+    def remove(self , element):
+        temp = self.head
+
+        if temp != None:
+            if temp.get == element:
+                self.head = temp.next
+                temp = None
+                return
+        while temp != None:
+            if temp.get == element:
+                break
+            prev = temp
+            temp = temp.next
+        if temp == None:
+            return
+        prev.next = temp.next
+        temp = None
+
 
 
 
 
 
 a = LinkedList()
-a.node(4)
-a.see_head()
-a.node(5)
-a.node(6)
+a.head = Node(1)
+e2 = Node(2)
+e3 = Node(3)
+a.head.next = e2
+e2.next = e3
+#a.display()
+a.insert(5)
+#a.display()
+a.append(10)
+a.custom_insert(11,a.head.next)
+#a.display()
+a.remove(5)
 a.display()
-a.next()
-a.next()
-a.next()
